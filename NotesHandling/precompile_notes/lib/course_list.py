388c6871a416e0cfa7227c9dc57cc6346586ed3c
JoachimFavre <joachim@favre.xyz>
2025-01-30 23:40:49 +0100

    Create CourseList enumeration

---
+++ NotesHandling/precompile_notes/lib/course_list.py
@@ -0,0 +1,38 @@
+from lib.course import Course
+
+class CourseList:
+    # BA1
+    ANALYSE_1 = Course(is_bachelor=True, semester=1, name="Analyse-1")
+    ALGEBRE_LINEAIRE = Course(is_bachelor=True, semester=1, name="AlgebreLinaire")
+    AICC_1 = Course(is_bachelor=True, semester=1, name="AICC-1")
+
+    # BA2
+    AICC_2 = Course(is_bachelor=True, semester=2, name="AICC-2")
+    ANALYSE_2 = Course(is_bachelor=True, semester=2, name="Analyse-2")
+    ANALYSE_2_METHODES_DE_DEMONSTRATION = Course(is_bachelor=True, semester=2, name="Analyse-2-MethodesDeDemonstration")
+    DIGITAL_SYSTEM_DESIGN = Course(is_bachelor=True, semester=2, name="DigitalSystemDesign")
+    
+    # BA3
+    ALGORITHMS = Course(is_bachelor=True, semester=3, name="Algorithms")
+    ANALYSE_3 = Course(is_bachelor=True, semester=3, name="Analyse-3")
+    COMPUTER_NETWORKS = Course(is_bachelor=True, semester=3, name="ComputerNetworks")
+    INTRO_TO_MACHINE_LEARNING_BA3 = Course(is_bachelor=True, semester=3, name="IntroToMachineLearning-BA3-Summary", is_summary=True)
+    NUMERICAL_METHODS = Course(is_bachelor=True, semester=3, name="NumericalMethods")
+    
+    # BA4
+    ANALYSE_4 = Course(is_bachelor=True, semester=4, name="Analyse-4")
+    PROBABILITY_AND_STATISTICS = Course(is_bachelor=True, semester=4, name="ProbabilityAndStatistics")
+    SIGNALS_AND_SYSTEMS = Course(is_bachelor=True, semester=4, name="SignalsAndSystems")
+    THEORY_OF_COMPUTATION = Course(is_bachelor=True, semester=4, name="TheoryOfComputation")
+    
+    # BA5
+    ALGEBRA = Course(is_bachelor=True, semester=5, name="Algebra")
+    INTRO_TO_QUANTUM_INFORMATION_PROCESSING = Course(is_bachelor=True, semester=5, name="IntroToQuantumInformationProcessing-Summary", is_summary=True)
+    
+    # BA6
+    INTRO_TO_QUANTUM_COMPUTATION = Course(is_bachelor=True, semester=6, name="IntroToQuantumComputation-Summary", is_summary=True)
+    
+    # MA1
+    ALGORITHMS_2 = Course(is_bachelor=False, semester=1, name="Algorithms-2")
+    COMPUTATIONAL_COMPLEXITY = Course(is_bachelor=False, semester=1, name="ComputationalComplexity")
+    QUANTUM_PHYSICS_2 = Course(is_bachelor=False, semester=1, name="QuantumPhysics-2")
--- NotesHandling/precompile_notes/main.py
+++ NotesHandling/precompile_notes/main.py
@@ -27,46 +27,47 @@ from typing import List
 from lib.precompiled_files.precompiled_summary import PrecompiledSummary
 from lib.tags import Tags
 from lib.course import Course
+from lib.course_list import CourseList
 from lib.logger import Logger
 from lib.precompiled_files import PrecompiledLectureGroup
 
 
 courses: List[Course] = [
     # BA1
-    #Course(is_bachelor=True, semester=1, name="Analyse-1"),
-    #Course(is_bachelor=True, semester=1, name="AlgebreLinaire"),
-    #Course(is_bachelor=True, semester=1, name="AICC-1"),
+    #CourseList.ANALYSE_1,
+    #CourseList.ALGEBRE_LINEAIRE,
+    #CourseList.AICC_1,
 
     # BA2
-    #Course(is_bachelor=True, semester=2, name="AICC-2"),
-    #Course(is_bachelor=True, semester=2, name="Analyse-2"),
-    #Course(is_bachelor=True, semester=2, name="Analyse-2-MethodesDeDemonstration"),
-    #Course(is_bachelor=True, semester=2, name="DigitalSystemDesign"),
+    #CourseList.AICC_2,
+    #CourseList.ANALYSE_2,
+    #CourseList.ANALYSE_2_METHODES_DE_DEMONSTRATION,
+    #CourseList.DIGITAL_SYSTEM_DESIGN,
     
     # BA3
-    #Course(is_bachelor=True, semester=3, name="Algorithms"),
-    #Course(is_bachelor=True, semester=3, name="Analyse-3"),
-    #Course(is_bachelor=True, semester=3, name="ComputerNetworks"),
-    #Course(is_bachelor=True, semester=3, name="IntroToMachineLearning-BA3-Summary", is_summary=True),
-    #Course(is_bachelor=True, semester=3, name="NumericalMethods"),
+    #CourseList.ALGORITHMS,
+    #CourseList.ANALYSE_3,
+    #CourseList.COMPUTER_NETWORKS,
+    #CourseList.INTRO_TO_MACHINE_LEARNING_BA3,
+    #CourseList.NUMERICAL_METHODS,
     
     # BA4
-    #Course(is_bachelor=True, semester=4, name="Analyse-4"),
-    #Course(is_bachelor=True, semester=4, name="ProbabilityAndStatistics"),
-    #Course(is_bachelor=True, semester=4, name="SignalsAndSystems"),
-    Course(is_bachelor=True, semester=4, name="TheoryOfComputation"),
+    #CourseList.ANALYSE_4,
+    #CourseList.PROBABILITY_AND_STATISTICS,
+    #CourseList.SIGNALS_AND_SYSTEMS,
+    #CourseList.THEORY_OF_COMPUTATION,
     
     # BA5
-    #Course(is_bachelor=True, semester=5, name="Algebra"),
-    #Course(is_bachelor=True, semester=5, name="IntroToQuantumInformationProcessing-Summary", is_summary=True),
+    #CourseList.ALGEBRA,
+    #CourseList.INTRO_TO_QUANTUM_INFORMATION_PROCESSING,
     
     # BA6
-    #Course(is_bachelor=True, semester=6, name="IntroToQuantumComputation-Summary", is_summary=True),
+    #CourseList.INTRO_TO_QUANTUM_COMPUTATION,
     
     # MA1
-    #Course(is_bachelor=False, semester=1, name="Algorithms-2"),
-    #Course(is_bachelor=False, semester=1, name="ComputationalComplexity"),
-    #Course(is_bachelor=False, semester=1, name="QuantumPhysics-2"),
+    #CourseList.ALGORITHMS_2,
+    #CourseList.COMPUTATIONAL_COMPLEXITY,
+    #CourseList.QUANTUM_PHYSICS_2,
 ]
 
 tag = Tags.NONE
